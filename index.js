434756d60c30d979e5f96d5d2c9fc592bd7d3f7e
unknown <tetiana.lisina@gmail.comgit config --global user.name T8886>
2022-09-28 02:14:20 -0500

    Updated

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 const inquirer = require('inquirer');
-const fs = require('fs'); 
-var people = []           
+const fs = require('fs');
+var people = [];
 var html = ""
-// const employee = require('./Employee');
-// const engineer = require("./engineer");
-// const manager = require('./manager');
-// const intern = require('./intern');
+const Engineer = require("./lib/Engineer");
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const generateWebPage = require('./generateWebPage');
 
-async function engineerQuestions() {
-   return await inquirer.prompt([
-        {
-            type: "list",
-            message: "Select title",
-            name: "Title",
-            choices: [
-                'Manager',
-                'Engineer',
-                'Intern',
-            ]
-        },
+// Manager's input
+const managerInfo = () => {
+    return inquirer.prompt([
         {
             type: "input",
             message: "Enter your name",
@@ -26,68 +17,121 @@ async function engineerQuestions() {
         },
         {
             type: "input",
-            message: "Enter your employee ID",
-            name: "Name",
+            message: "Enter your ID",
+            name: "ID",
         },
         {
             type: "input",
             message: "Enter your email address",
-            name: "Name",
+            name: "Email",
+        },
+        {
+            type: "input",
+            message: "Enter your office number",
+            name: "Office",
         },
 
     ])
-        .then((answers) =>
-            fs.writeFile('index.html', generateMarkdown(answers), (err) =>
-                err ? console.log(err) : console.log("Sucess!")
-            )
-        );
+        .then((answers) => {
+            const manager = new Manager[answers.Name, answers.ID, answers.Email, answers.Office];
+            people.push(manager);
+            chooseEmployee();
+        })
 }
 
-
-inquirer.prompt([
-    {
-        type: "input",
-        message: "Enter manager's name",
-        name: "Name",
-    },
-    {
-        type: "input",
-        message: "Enter manager's employee ID",
-        name: "ID",
-    },
-    {
-        type: "input",
-        message: "Enter manager's email address",
-        name: "Email",
-    },
-    {
-        type: "input",
-        message: "Enter manager's office number",
-        name: "Office",
-    },
-    {
-        type: "list",
-        message: "Select title",
-        name: "Adding",
-        choices: [
-            'Add Engineer',
-            'Add Intern',
-            'Finish building team'
-        ]
-    },
-])
-    .then((answers) => {
-        if (answers.Adding == "Add Engineer") {
-
-        } else if (answers.Adding == "Add Intern") {
-
-        } else {
-
-            fs.writeFile('index.html', html, (err) =>
-                err ? console.log(err) : console.log("Sucess!")
-            )
+// Options if add more emp or finish
+const chooseEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            message: "Select one of the following options ",
+            name: "Adding",
+            choices: [
+                'Add Engineer',
+                'Add Intern',
+                'Finish building team'
+            ]
         }
+    ])
+        .then(pickedOption => {
+            switch (pickedOption.key) {
+                case "Add Engineer":
+                    engineerInfo();
+                    break;
+                case "Add Intern":
+                    internInfo();
+                    break;
+                default:
+                    buildTeam();
+            }
+        });
+};
 
+// Engineer's input
+const engineerInfo = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter engineer's name",
+            name: "Name",
+        },
+        {
+            type: "input",
+            message: "Enter engineer's employee ID",
+            name: "ID",
+        },
+        {
+            type: "input",
+            message: "Enter Engineer's email address",
+            name: "Email",
+        },
+        {
+            type: "input",
+            message: "Enter Engineer's Github",
+            name: "Github",
+        },
+    ])
+        .then((answers) => {
+            const engineer = new Engineer[answers.Name, answers.ID, answers.Email, answers.Github];
+            people.push(engineer);
+            chooseEmployee();
+        })
+};
 
+// Intern's Input
+const internInfo = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter intern's name",
+            name: "Name",
+        },
+        {
+            type: "input",
+            message: "Enter intern's employee ID",
+            name: "ID",
+        },
+        {
+            type: "input",
+            message: "Enter inter's email address",
+            name: "Email",
+        },
+        {
+            type: "input",
+            message: "Enter inter's school",
+            name: "School",
+        },
+    ])
+        .then((answers) => {
+            const intern = new Intern[answers.Name, answers.ID, answers.Email, answers.School];
+            people.push(intern);
+            chooseEmployee();
+        })
+};
 
-    }); 
+// Building webpage
+const buildTeam = ()=>{
+    fs.writeFileSync(outputPath, generateWebPage(people), "utf-8")
+}
+
+managerInfo()
